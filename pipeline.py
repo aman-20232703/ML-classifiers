7b241cf72159783657ee1b4096c9e24da36f6f47
aman-20232703 <ramanujancollege2703@gmail.com>
2025-07-21 23:24:43 +0530

    initial commit

---
+++ pipeline.py
@@ -0,0 +1,23 @@
+# from sklearn.datasets import load_iris
+# iris=load_iris()
+
+# x=iris.data
+# y=iris.target
+# # print(x,y)
+
+# from sklearn.model_selection import train_test_split
+# x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.33)
+# # print("X_train:", x_train, "X_test:", x_test)
+# # print("y_train:", y_train, "y_test:", y_test)
+
+# from sklearn import tree
+# my_classifier=tree.DecisionTreeClassifier()
+
+# my_classifier.fit(x_train, y_train)
+
+# predictions=my_classifier.predict(x_test)
+# print(predictions)
+
+# from sklearn.metrics import accuracy_score
+# accuracy=accuracy_score(y_test, predictions)
+# print(accuracy)
